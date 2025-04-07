4cd12fb5438c05c6980c406185dfb777bdc29965
SnehaJose-dotcom <snehajose0608@gmail.com>
2025-04-07 18:18:51 -0400

    Delete angularClientApp directory

--- angularClientApp/src/main.ts
+++
@@ -1,9 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
-import { HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
-
-bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(HttpClientModule), ...appConfig.providers]
-});
